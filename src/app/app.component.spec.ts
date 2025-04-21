3e84146dbb45f39bf27cfa6de47c07dd8c859161
Myrmex <dfusi@hotmail.com>
2025-04-21 09:37:54 +0200

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'cadmus-demo-app' title`, () => {
+  it(`should have the 'cadmus-shell-v3' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cadmus-demo-app');
+    expect(app.title).toEqual('cadmus-shell-v3');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, cadmus-demo-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, cadmus-shell-v3');
   });
 });
